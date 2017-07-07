cba60fae9e57d0b96d61c8741a2b9223ab8ae46d
Aviv Levitski <AvivLevitski@avivlevitski.local>
2017-07-07 17:25:14 +0300

    Initial Commit

---
+++ Mmn14/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Mmn14
+//
+//  Created by Aviv Levitski on 7.7.2017.
+//  Copyright © 2017 Aviv Levitski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
