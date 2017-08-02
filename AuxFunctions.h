abf9e2b95d5455e6806d91248e97e7d6e37e32db
avivlevizky <avivlevizky@gmail.com>
2017-08-02 23:31:33 +0300

    Update AuxFunctions.h

--- AuxFunctions.h
+++ AuxFunctions.h
@@ -11,15 +11,19 @@ int isValidLabel(char * label);
 /*Bool Function that check if the first string from the command line is a lavel or not*/
 int isLabel(char * order);
 
-/*int Function that checks if the given order string is an vald instruction/data defining order: if is .data then ->16, if is .string then ->17, if is .mat then ->18 ,if is .entry then -> 19, if is .extern then -> 20 ,if is mov ->func.mov,otherwise the function will return -1;
- if EntryExter=0 then the function will checks without checking entry and extern otherwise the function will checks all the options above including .entry and .extern*/
-int isInstruction(char * order, int EntryExtern);
+/*int Function that checks if the given order string is an vald instruction/data defining order: if is .data then ->16, if is .string then ->17, if is .mat then ->18 ,if is .entry then -> 19, if is .extern then -> 20 ,if is mov ->func.mov,otherwise the function will return -1*/
+int isInstruction(char * order);
 
 /*Function that insert the symbol into the symbols tabel by the type: 16 or 17 or 18 is data;19 is .entry and 20 is .extern otherwise is instruction type. and update SC counter too*/
-void insertSymbolToTable(char *label,int type);
+void insertSymbolToTable(char **data,int type);
 
 /* Function that insert the given data into the instructions_table, the function will update the IC counter by matching to the length of the given data */
 void insertToIT(char **data,int Instruc_type);
+
+
+/*Function that insert data by the given Instruc_type argument into the data table*/
+void insertToDT(char **data,int Instruc_type);
+
 
 
 /*Update the given instruction command for the second checking*/
