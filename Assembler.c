e60726886cf98c90d63d547b791e7eb6ba55233f
avivlevizky <avivlevizky@gmail.com>
2017-08-02 21:52:55 +0300

    Update Assembler.c

--- Assembler.c
+++ Assembler.c
@@ -64,9 +64,9 @@ void freeLinkedList(char ** list)
 
 
 /*get the whole command and transfer it to linked list, if returned ans>0 then there is an error in the input otherwise the input is valid*/
-void CommandLineToLinkedList()
+void CommandLineToLinkedList(int NumIteration)
 {
-    char ** commands;           /*dynamic matrix of strings*/
+    char ** command;           /*dynamic matrix of strings*/
     char c;              /*char variable to iterate on content std*/
     int chars_len,word_counter;      /*chars_len: the char length of the current word, word_counter: indicate in the current number of word*/
     
@@ -74,19 +74,19 @@ void CommandLineToLinkedList()
     word_counter=0;
     c='\0';
     chars_len=1;
-    commands=(char **)malloc(sizeof(char *));
-    allocate_check((char **)commands);            /*-------------Need to check if (char **)commands is valid------------*/
+    command=(char **)malloc(sizeof(char *));
+    allocate_check((char **)command);            /*-------------Need to check if (char **)commands is valid------------*/
     commands[0]=(char *)calloc(1,sizeof(char));
-    allocate_check((char *)commands[0]);
+    allocate_check((char *)command[0]);
     
     while(((c=fgetc(fp))!=EOF)&&(c!='\n'))
     {
         if((c!=' ')&&(c!=','))
         {
-            commands[word_counter]=(char *)realloc((char *)(commands[word_counter]), (chars_len+1)*sizeof(char));
-            allocate_check((char *)commands[word_counter]);
-            commands[word_counter][chars_len-1]=c;
-            commands[word_counter][chars_len]='\0';
+            command[word_counter]=(char *)realloc((char *)(command[word_counter]), (chars_len+1)*sizeof(char));
+            allocate_check((char *)command[word_counter]);
+            command[word_counter][chars_len-1]=c;
+            command[word_counter][chars_len]='\0';
             chars_len++;
         }
        else
@@ -94,10 +94,10 @@ void CommandLineToLinkedList()
            if(chars_len>1)
            {
                word_counter++;
-               commands=(char **)realloc((char **)commands, (word_counter+1)*sizeof(char *));
-               allocate_check((char **)commands);
-               commands[word_counter]=(char *)calloc(1,sizeof(char));
-               allocate_check((char *)commands[word_counter]);
+               command=(char **)realloc((char **)command, (word_counter+1)*sizeof(char *));
+               allocate_check((char **)command);
+               command[word_counter]=(char *)calloc(1,sizeof(char));
+               allocate_check((char *)command[word_counter]);
                chars_len=1;
            }
           
@@ -105,14 +105,20 @@ void CommandLineToLinkedList()
     }
     if(c=='\n')
     {
-        CheckingCommand(&commands);
-        freeLinkedList(&commands);
-        CommandLineToLinkedList();
+        if(NumIteration)
+            FirstCheckingCommand(&command);
+        else
+            CompleteCheckingCommand(&command);
+        freeLinkedList(&command);
+        CommandLineToLinkedList(NumIteration);
     }
     else   /*if c is EOF*/
     {
-        CheckingCommand(&commands);
-        freeLinkedList(&commands);
+        if(NumIteration)
+            FirstCheckingCommand(&command);
+        else
+            CompleteCheckingCommand(&command);
+        freeLinkedList(&command);
     }
 }
 
@@ -122,37 +128,43 @@ void CommandLineToLinkedList()
 
 
 /*checking and processing the current command line*/
-void CheckingCommand(char ** commands)
+void FirstCheckingCommand(char ** command)
 {
     int flag_symbol_type;
 
-    if (isLabel(commands[0]))
+    if (isLabel(command[0]))
     {
         /*In the case that the first string on the current command line is a label(symbol) */
-        if((isValidLabel(commands[0]))&&((flag_symbol_type=isInstruction(commands[1],0)>=0)))
+        if((isValidLabel(command[0]))&&((flag_symbol_type=isInstruction(command[1],0)>=0)))
         {
             /*In the case that the current label is a valid label*/
-            insertSymbolToTable(commands[0],flag_symbol_type);
-            insertToIT(&commands[2],flag_symbol_type);   /*the commands[2] is first operand*/
+            insertSymbolToTable(command[0],flag_symbol_type);
+            insertToIT(&command[2],flag_symbol_type);   /*the command[2] is first operand*/
         }
     }
     else /*if the commands[0] isn't label*/
     {
-        if ((flag_symbol_type=isInstruction(commands[0],1)>=0))
+        if ((flag_symbol_type=isInstruction(command[0],1)>=0))
         {
             if (flag_symbol_type>=19)
             {
-                insertSymbolToTable(commands[1],flag_symbol_type);
+                insertSymbolToTable(command[1],flag_symbol_type);
             }
             else
             {
-                insertToIT(&commands[1],flag_symbol_type);  /*the commands[1] is first operand*/
+                insertToIT(&command[1],flag_symbol_type);  /*the command[1] is first operand*/
             }
-        }
-        
+        }    
     }
+}
+
+/*The Secound check of the given command line*/
+void SecondCheckingCommand(char ** command)
+{
     
 }
+
+
 
 
 
@@ -173,7 +185,7 @@ int main(int argc,char * argv) {
         exit(0);
     }
     
-    CommandLineToLinkedList();
+    CommandLineToLinkedList(1);
     
     /*sets the file position to the beginning of the assembly file*/
     rewind(fp);
@@ -183,7 +195,12 @@ int main(int argc,char * argv) {
         /*Print all the compile error from ErrorsAssembler*/
     }
     
-    /*Second pass of the program*
+    /*Second pass of the program*/
+    IC=0;
+    LC=0;
+    CommandLineToLinkedList(2);
+
+    
     
     return 0;
     
